d1bdbcf2097e5dcaacd7c1a6dc4a4e05f9de4ca0
Cedric Lee Shao Ming <cedric.lee.2018@sis.smu.edu.sg>
2020-10-09 18:41:43 +0800

    Add firebase boilerplate

---
+++ backend/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCSkv9frykkkauMEX0QInufgsEWpfLvwwg",
+    authDomain: "forchange-7d138.firebaseapp.com",
+    databaseURL: "https://forchange-7d138.firebaseio.com",
+    projectId: "forchange-7d138",
+    storageBucket: "forchange-7d138.appspot.com",
+    messagingSenderId: "826393963371",
+    appId: "1:826393963371:web:d9e8ea84cde28423b7ba2e"
+  };
+
+  firebase.initializeApp(firebaseConfig);
