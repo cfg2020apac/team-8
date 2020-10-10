b8baaa8cafdf692e49f98e9ef0bcb911690da114
Cedric Lee Shao Ming <cedric.lee.2018@sis.smu.edu.sg>
2020-10-10 09:51:26 +0800

    Update index.js

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -3,11 +3,6 @@ const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
 
-// const serviceAccount = require("./for-change-292011-e309d6a3b734.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
 admin.initializeApp();
 const db = admin.firestore();
 
@@ -20,18 +15,6 @@ app.get("/", (req, res) => {
   return res.status(200).send("Hello World!");
 });
 
-// app.post("/write-doc", async (req, res) => {
-//   const { collection, documentId, documentValue } = req.body;
-//   const docRef = db.collection(collection).doc(documentId);
-//   try {
-//     const r = await docRef.set(documentValue);
-//     console.log("Write Success", r);
-//     return res.status(200).end();
-//   } catch (e) {
-//     console.error("Write Failure", r);
-//     return res.status(500).end();
-//   }
-// });
 
 app.post("/add-volunteer", async (req, res) => {
   const { userDetails } = req.body;
